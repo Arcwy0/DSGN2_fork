be1dd5d5a41351d15e86d7b51184b62c5f4ab815
Zion-Go2 <slxx5237@gmail.com>
2023-07-04 13:39:31 +0300

    No idea

--- docker/scripts/build.sh
+++ docker/scripts/build.sh
@@ -12,7 +12,7 @@ export PRODUCTION_PATH=$PWD
 export ARCH=`uname -m`
 export NUM_THREADS=`nproc`
 
-docker-compose --env-file $PRODUCTION_PATH/build.env \
+docker compose --env-file $PRODUCTION_PATH/build.env \
     -f $PRODUCTION_PATH/build.yml \
     up --build $@
 
